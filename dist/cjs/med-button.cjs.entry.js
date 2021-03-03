abf4abc9865f87ec51ab6c0c204074000b044076
alexbleggi <alexbleggi@gmail.com>
2021-03-03 11:13:40 -0300

    Implementação v1

---
+++ dist/cjs/med-button.cjs.entry.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const index = require('./index-2a75be79.js');
+
+const iosMedButtonCss = ":host h1{font-size:25px;color:#fff}";
+
+const mdMedButtonCss = ":host h1{font-size:25px;color:#fff}";
+
+const MedButton = class {
+  constructor(hostRef) {
+    index.registerInstance(this, hostRef);
+  }
+  render() {
+    return (index.h(index.Host, null, index.h("h1", null, "MED-BUTTON")));
+  }
+};
+MedButton.style = {
+  ios: iosMedButtonCss,
+  md: mdMedButtonCss
+};
+
+exports.med_button = MedButton;
