fd6691d8d14bfc4750b789dc51bf9947437034d1
alexbleggi <alexbleggi@gmail.com>
2021-09-23 16:39:39 -0300

    Ajustes

---
+++ dist/types/components/@medgrupo/cores/parent/parent.d.ts
@@ -0,0 +1,16 @@
+import { Color } from '../../../../interface';
+export declare class Parent {
+  /**
+    * Define a cor do componente.
+    */
+  dsColor?: Color;
+  /**
+    * Define o fill do componente.
+    */
+  fill?: 'outline';
+  /**
+    * Teste.
+    */
+  child: boolean;
+  render(): any;
+}
---
+++ dist/types/components/@medgrupo/team/questoes/med-option/med-option.d.ts
@@ -0,0 +1,8 @@
+import { MedColor } from '../../../../../interface';
+export declare class MedOption {
+  /**
+    * Define a cor do componente.
+    */
+  dsColor?: MedColor;
+  render(): any;
+}
