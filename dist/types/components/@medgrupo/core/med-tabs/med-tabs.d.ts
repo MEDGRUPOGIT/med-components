52be3ebda448a0f3c9ba558b07ad677a56fee6a6
alexbleggi <alexbleggi@gmail.com>
2021-12-20 17:14:22 -0300

    ajustes

--- dist/types/components/@medgrupo/core/med-tabs/med-tabs.d.ts
+++
@@ -1,8 +0,0 @@
-import { MedColor } from '../../../../interface';
-export declare class MedTabs {
-  /**
-    * Define a cor do componente.
-    */
-  dsColor?: MedColor;
-  render(): any;
-}
--- dist/types/components/@medgrupo/team/questoes/med-cartao-resposta-lista/med-cartao-resposta-lista.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class MedCartaoRespostaLista {
-  render(): any;
-}
